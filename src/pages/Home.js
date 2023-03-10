c6df202772c63c7577ae38abdbf25c7d257f71a1
gio <gio>
2023-03-10 18:57:37 +0800

    add list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,24 @@
+import { useState, useEffect } from "react";
+
 function Home() {
-  return <div>I'm Home!</div>;
+  const [list, setList] = useState([
+    { id: 1, title: "Gio", description: "Smartest coder" },
+    { id: 2, title: "Gita", description: "Cutest adventurer" },
+    { id: 3, title: "Marco", description: "Best filmamker" },
+  ]);
+
+  return (
+    <div>
+      <div className="">
+        {list.map((item) => (
+          <div className="" key={item.id}>
+            <p>{item.title}</p>
+            <p>{item.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
