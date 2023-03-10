bf690f457414d13904d3e71541324b409461cb77
gio <gio>
2023-03-10 19:09:27 +0800

    connect HomeJS to db.json

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,26 @@
 import { useState, useEffect } from "react";
 
 function Home() {
-  const [list, setList] = useState([
-    { id: 1, title: "Gio", description: "Smartest coder" },
-    { id: 2, title: "Gita", description: "Cutest adventurer" },
-    { id: 3, title: "Marco", description: "Best filmamker" },
-  ]);
+  const [list, setList] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/cards")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => setList(data));
+  }, []);
 
   return (
     <div>
       <div className="">
-        {list.map((item) => (
-          <div className="" key={item.id}>
-            <p>{item.title}</p>
-            <p>{item.description}</p>
-          </div>
-        ))}
+        {list &&
+          list.map((item) => (
+            <div className="" key={item.id}>
+              <p>{item.title}</p>
+              <p>{item.description}</p>
+            </div>
+          ))}
       </div>
     </div>
   );
